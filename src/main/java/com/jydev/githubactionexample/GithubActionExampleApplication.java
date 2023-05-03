bf509915e3df7133bb530e58fc98ba84f5e5227f
jaeyoung <kjy0302014@gmail.com>
2023-05-03 13:07:20 +0900

    Test

--- src/main/java/com/jydev/githubactionexample/GithubActionExampleApplication.java
+++ src/main/java/com/jydev/githubactionexample/GithubActionExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionExampleApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(GithubActionExampleApplication.class, args);
     }
 
